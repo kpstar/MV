284eac0356716023ec57a235e064115cb6c76afa
kpstar <kpstar.mc@gmail.com>
2021-12-23 08:42:27 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,531 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+		<meta name="description" content="" />
+		<meta name="author" content="" />
+		<title>Makrverse</title>
+		<link rel="icon" type="image/x-icon" href="assets/img/fav.ico" />
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet"/>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link href="assets/css/styles.css" rel="stylesheet" />
+	</head>
+	<body id="page-top">
+		<!-- Navigation-->
+		<nav
+			class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm"
+			id="mainNav"
+		>
+			<div class="container px-5">
+				<a class="navbar-brand fw-bold" href="./index.php#page-top">
+					<img
+						src="./assets/img/logoFull.png"
+						alt="Makrverse logo"
+						width="150rem"
+						draggable="false"
+					/>
+				</a>
+				<button
+					class="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarResponsive"
+					aria-controls="navbarResponsive"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					Menu
+					<i class="bi-list"></i>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+						<li class="nav-item">
+							<a class="nav-link me-lg-3" href="#features">
+							    <i class="bi-card-list me-2"></i>
+							    App
+						    </a>
+						</li>
+
+    					<li class="nav-item">
+    						<a class="nav-link me-lg-3" href="#partnerships">
+    						    <i class="bi-people-fill me-2"></i>
+    						    Partnerships
+    						</a>
+    					</li>
+    					<li class="nav-item">
+    						<a class="nav-link me-lg-3" href="#media">
+    						    <i class="bi-camera-fill me-2"></i>
+    						    Media
+    						</a>
+    					</li>
+    					
+    					<button
+    						class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0"
+    						data-bs-toggle="modal"
+    						onclick=" window.open('./gpicerace.php','_blank')">
+    					
+    						<span class="d-flex align-items-center">
+    				    		<i class="bi-trophy-fill me-2"></i>
+    							<span class="small">GP ICE Race</span>
+    						</span>
+    					</button>
+    				</ul>
+				</div>
+			</div>
+		</nav>
+		<!-- Mashead header-->
+		<header class="masthead">
+			<div class="container px-5">
+			    <div class="row gx-5 align-items-center">
+			    <div class="col-lg-6 mb-5">
+						<!-- Masthead device mockup feature-->
+						<div class="masthead-device-mockup">
+							<svg
+								class="circle"
+								viewBox="0 0 100 100"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<defs>
+									<linearGradient
+										id="circleGradient"
+										gradientTransform="rotate(45)"
+									>
+										<stop class="gradient-start-color" offset="0%"></stop>
+										<stop class="gradient-end-color" offset="100%"></stop>
+									</linearGradient>
+								</defs>
+								<circle cx="50" cy="50" r="50"></circle>
+							</svg>
+							<svg
+								class="shape-1 d-none d-sm-block"
+								viewBox="0 0 240.83 240.83"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<rect
+									x="-32.54"
+									y="78.39"
+									width="305.92"
+									height="84.05"
+									rx="42.03"
+									transform="translate(120.42 -49.88) rotate(45)"
+								></rect>
+								<rect
+									x="-32.54"
+									y="78.39"
+									width="305.92"
+									height="84.05"
+									rx="42.03"
+									transform="translate(-49.88 120.42) rotate(-45)"
+								></rect>
+							</svg>
+							<svg
+								class="shape-2 d-none d-sm-block"
+								viewBox="0 0 100 100"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<circle cx="50" cy="50" r="50"></circle>
+							</svg>
+							<div class="device-wrapper">
+								<div
+									class="device"
+									data-device="iPhoneX"
+									data-orientation="portrait"
+									data-color="black"
+								>
+									<div class="screen bg-black">
+										 <video
+											muted="muted"
+											autoplay=""
+											playsinline
+											loop=""
+											style="max-width: 100%; height: 100%"
+										>
+											<source src="assets/video/demo2.mp4" type="video/mp4" />
+										</video>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+					<div class="col-lg-6">
+						<!-- Mashead text and app badges-->
+						<div class="mb-5 mb-lg-0 text-center text-lg-start">
+							<h1 class="display-1 lh-1 mb-3" style="font-weight: bold">
+								Discover awesome inventions!
+							</h1>
+							<p class="lead fw-normal text-muted mb-5">
+								Makrverse is a social media marketplace for inventors and tech enthusiasts.
+							</p>
+							<div class="d-flex flex-column flex-lg-row align-items-center">
+								<a class="me-lg-3 mb-4 mb-lg-0" href="https://play.google.com/store/apps/details?id=com.makrverse.makrverse"
+									><img
+										class="app-badge-large"
+										src="assets/img/google-play-badge.svg"
+										alt="..."
+								/></a>
+								<a class="me-lg-3 mb-4 mb-lg-0" href="https://apps.apple.com/gb/app/makrverse/id1593041971"
+									><img
+										class="app-badge-large"
+										src="assets/img/app-store-badge.svg"
+										alt="..."
+								/></a>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+		</header>
+		<!-- Quote/testimonial aside-->
+		<div id="carouselExampleControls" class="carousel carousel-dark slide" data-bs-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+                <aside class="text-center bg-white">
+        			<div class="container px-5">
+        				<div class="row gx-5 justify-content-center">
+        					<div class="col-xl-8">
+        						<div class="h2 fs-1 text-black mb-5">
+        							<!-- <div class="h2 fs-1 text-white mb-4"> -->
+        							"This team never ceased to amaze us! Their creativity and
+        							talent made their pitch one of the most impressive"
+        						</div>
+        						<img
+        							src="assets/img/snc.png"
+        							alt="Startup Nation Challenge"
+        							style="height: 4rem"
+        						/>
+        					</div>
+        				</div>
+        			</div>
+        		</aside>
+            </div>
+            <div class="carousel-item">
+                <aside class="text-center bg-white">
+        			<div class="container px-5">
+        				<div class="row gx-5 justify-content-center">
+        					<div class="col-xl-8">
+        						<div class="h2 fs-1 text-black mb-5">
+        							"I am proud that I was the mentor of Makrverse during their initial stage, and I can't wait to see them fulfil their potential"
+        						</div>
+        						<div class="h4 fs-3 mb-5" style="color: #316bea">
+        							Alex Bouaziz, CEO & Co-Founder of Deel
+        						</div>
+        					</div>
+        				</div>
+        			</div>
+        		</aside>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+		<!-- App features section-->
+		<section id="features">
+			<div class="container px-5">
+				<div class="row gx-5 align-items-center">
+					<div class="col-lg-8 order-lg-1 mb-5 mb-lg-0">
+						<div class="container-fluid px-5">
+							<div class="row gx-5">
+								<div class="col-md-6 mb-5">
+									<!-- Feature item-->
+									<div class="text-center">
+										<i
+											class="
+												bi bi-camera-fill
+												icon-feature
+												text-gradient
+												d-block
+												mb-3
+											"
+										></i>
+										<h3 class="font-alt">Share</h3>
+										<p class="text-muted mb-0">
+											Stand out with your latest inventions!
+										</p>
+									</div>
+								</div>
+								<div class="col-md-6 mb-5">
+									<!-- Feature item-->
+									<div class="text-center">
+										<i
+											class="bi-tools icon-feature text-gradient d-block mb-3"
+										></i>
+										<h3 class="font-alt">Request</h3>
+										<p class="text-muted mb-0">
+											Want something? Someone out there can make it for you!
+										</p>
+									</div>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-md-6 mb-5 mb-md-0">
+									<!-- Feature item-->
+									<div class="text-center">
+										<i
+											class="
+												bi bi-people-fill
+												icon-feature
+												text-gradient
+												d-block
+												mb-3
+											"
+										></i>
+										<h3 class="font-alt">Explore</h3>
+										<p class="text-muted mb-0">
+											Network and interact with other creative minds!
+										</p>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<!-- Feature item-->
+									<div class="text-center">
+										<i
+											class="
+												bi bi-gift-fill
+												icon-feature
+												text-gradient
+												d-block
+												mb-3
+											"
+										></i>
+										<h3 class="font-alt">Exchange</h3>
+										<p class="text-muted mb-0">
+											Discover your true potential through our marketplace!
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-4 order-lg-0">
+						<!-- Features section device mockup-->
+						<div class="features-device-mockup">
+							<svg
+								class="circle"
+								viewBox="0 0 100 100"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<defs>
+									<linearGradient
+										id="circleGradient"
+										gradientTransform="rotate(45)"
+									>
+										<stop class="gradient-start-color" offset="0%"></stop>
+										<stop class="gradient-end-color" offset="100%"></stop>
+									</linearGradient>
+								</defs>
+								<circle cx="50" cy="50" r="50"></circle>
+							</svg>
+							<svg
+								class="shape-1 d-none d-sm-block"
+								viewBox="0 0 240.83 240.83"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<rect
+									x="-32.54"
+									y="78.39"
+									width="305.92"
+									height="84.05"
+									rx="42.03"
+									transform="translate(120.42 -49.88) rotate(45)"
+								></rect>
+								<rect
+									x="-32.54"
+									y="78.39"
+									width="305.92"
+									height="84.05"
+									rx="42.03"
+									transform="translate(-49.88 120.42) rotate(-45)"
+								></rect>
+							</svg>
+							<svg
+								class="shape-2 d-none d-sm-block"
+								viewBox="0 0 100 100"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<circle cx="50" cy="50" r="50"></circle>
+							</svg>
+							<div class="device-wrapper">
+								<div
+									class="device"
+									data-device="iPhoneX"
+									data-orientation="portrait"
+									data-color="black"
+								>
+									<div class="screen bg-black">
+										 <video
+											muted="muted"
+											autoplay=""
+											playsinline
+											loop=""
+											style="max-width: 100%; height: 100%"
+										>
+											<source src="assets/video/demo2.mp4" type="video/mp4" />
+										</video>
+										<!--<img style="max-width: 100%; height: 100%" src="./assets/img/splash2.png"/>-->
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Basic features section-->
+		<section class="bg-light">
+			<div class="container px-5">
+				<div
+					class="
+						row
+						gx-5
+						align-items-center
+						justify-content-center justify-content-lg-between
+					"
+				>
+					<div class="col-12 col-lg-5">
+						<h2 class="display-4 lh-1 mb-4 text-left">Enter a network of Makrs</h2>
+						<p class="lead fw-normal text-muted mb-5 mb-lg-0 text-left">
+							By being a part of Makrverse, you are joining a community of Makrs.
+							 Makrs are the tech enthusiasts who cannot wait to have their hands 
+							 on the newest inventions, the early adopters, the innovators. Makrverse is the 
+							 place for everyone who wants to be a part of a making universe.
+						</p>
+					</div>
+					<div class="col-sm-8 col-md-6">
+						<div class="px-5 px-sm-0">
+							<img
+								class="img-fluid rounded-circle"
+								src="./assets/img/1.jpg"
+								alt="Impact your community through homemade inventions"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="partnerships">
+			<div class="container px-5">
+				<div
+					class="
+						row
+						gx-5
+						align-items-center
+						justify-content-center justify-content-lg-between
+					"
+				>
+				    <div class="col-sm-8 col-md-6 mb-3">
+						<div class="px-5 px-sm-0">
+							<img
+								class="img-fluid rounded-circle"
+								src="./assets/img/partner.jpg"
+								alt="Impact your community through homemade inventions"
+							/>
+						</div>
+					</div>
+					<div class="col-12 col-lg-5">
+						<h2 class="display-4 lh-1 mb-4 text-left">Become a Makrverse Partner</h2>
+						<p class="lead fw-normal text-muted mb-5 mb-lg-0 text-left">
+    						Makrverse enables creators to become their own entrepeneurs. By becoming a partner you are promoting creations from its source. Makrverse is continuously growing, so if you want to grow with us contact us at 
+    						<a href="mailto:partner@makrverse.com">partner@makrverse.com</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="media" class="bg-light">
+			<div class="container px-5">
+				<div
+					class="
+						row
+						gx-5
+						align-items-center
+						justify-content-center justify-content-lg-between
+					"
+				>
+					<div class="col-12 col-lg-5">
+						<h2 class="display-4 lh-1 mb-4 text-left">Contact Makrverse from the media</h2>
+						<p class="lead fw-normal text-muted mb-5 mb-lg-0 text-left">
+    						Makrverse is increasing its presence every day; if you are interested in knowing how we came to be, where we are at or where are we going, contact us at
+    						<a href="mailto:media@makrverse.com">media@makrverse.com</a>
+						</p>
+					</div>
+					<div class="col-sm-8 col-md-6 mb-3">
+						<div class="px-5 px-sm-0">
+							<img
+								class="img-fluid rounded-circle"
+								src="./assets/img/media.jpg"
+								alt="Impact your community through homemade inventions"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+<!-- App badge section-->
+<section class="bg-gradient-primary-to-secondary" id="download">
+    <div class="container px-5">
+        <h2 class="text-center text-white font-alt mb-4">Get the app now!</h2>
+        <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
+            <a class="me-lg-3 mb-4 mb-lg-0" href="https://play.google.com/store/apps/details?id=com.makrverse.makrverse"><img class="app-badge" src="assets/img/google-play-badge.svg" alt="..." /></a>
+            <a href="https://apps.apple.com/gb/app/makrverse/id1593041971"><img class="app-badge" src="assets/img/app-store-badge.svg" alt="..." /></a>
+        </div>
+    </div>
+</section>
+<footer class="bg-body text-center py-5">
+	<div class="container px-5">
+		<div class="text-white-50 small">
+			<div class="mb-2">&copy; Makrverse 2021. All Rights Reserved.</div>
+			<a href="./privacy.php" target="_blank">Privacy</a>
+			<span class="mx-1">&middot;</span>
+			<a href="./terms.php" target="_blank">Terms</a>
+			<!--<span class="mx-1">&middot;</span>-->
+            <!--<a href="#!">FAQ</a> -->
+		</div>
+	</div>
+</footer>
+
+        <!-- Feedback Modal-->
+        <div
+            class="modal fade"
+            id="feedbackModal"
+            tabindex="-1"
+            aria-labelledby="feedbackModalLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header bg-gradient-primary-to-secondary p-4">
+                        <h5 class="modal-title font-alt text-white" id="feedbackModalLabel">
+                            Contact us: Press
+                        </h5>
+                        <button
+                            class="btn-close btn-close-white"
+                            type="button"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <div class="modal-body border-0 p-4">
+                        <h5 class="modal-title font-alt text-black">
+                            Email us at contact@makrverse.com
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
+		<script src="assets/js/scripts.js"></script>
+		<!-- Delete script below when using our own php to send form to our email -->
+		<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+	</body>
+</html>
